ece83c8a7a2b83de4aa0a420f3b16ae6b8a8c884
Aurelian POLEAC <aurelian.poleac@gmail.com>
2024-05-21 15:26:12 +0300

    Initial commit

---
+++ src/main/java/com/home/ttbooking/TtBookingApplication.java
@@ -0,0 +1,13 @@
+package com.home.ttbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/home/ttbooking/TtBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.ttbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
